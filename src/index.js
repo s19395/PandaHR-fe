9530b3cb588889fba3b3b2449b085be8c1411674
Oskar Tybura (s19395) <s19395@pjwstk.edu.pl>
2024-04-01 21:10:08 +0200

    Added theme.js and test app bar

--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 import { DevSupport } from '@react-buddy/ide-toolbox';
 import { ComponentPreviews, useInitial } from './dev';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import theme from './theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <DevSupport ComponentPreviews={ComponentPreviews}
-                useInitialHook={useInitial}
-    >
-      <App />
+    <DevSupport ComponentPreviews={ComponentPreviews} useInitialHook={useInitial}>
+      <CssBaseline />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
     </DevSupport>
   </React.StrictMode>
 );
