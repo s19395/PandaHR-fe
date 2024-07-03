a6fb3bb55d03732d13075af7be22a1985be095aa
Oskar Tybura (s19395) <s19395@pjwstk.edu.pl>
2024-07-03 23:16:19 +0200

    #19 Changed table to MUIDataTable

--- src/pages/Employees.js
+++ src/pages/Employees.js
@@ -1,54 +1,33 @@
 import * as React from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import { useAuth } from '../helper/AuthProvider';
+import MUIDataTable from 'mui-datatables';
 import { useEffect, useState } from 'react';
 import { request } from '../helper/AxiosHelper';
+import { useAuth } from '../helper/AuthProvider';
 
-export default function BasicTable() {
+export default function Employees() {
+  const columns = ['Imię', 'Nazwisko', 'Date of Birth'];
   const [data, setData] = useState([]);
   const { token } = useAuth(); // Get the token from the AuthProvider
 
   useEffect(() => {
     request('GET', '/employees/findAll', null, token)
       .then((response) => {
-        console.log(response.data);
-        setData(response.data);
+        const transformedData = response.data.map((employee) => [
+          employee.firstName,
+          employee.lastName,
+          employee.dateOfBirth
+        ]);
+        setData(transformedData);
       })
       .catch((error) => {
         console.error(error);
       });
   }, []);
+  const options = {
+    filterType: 'checkbox'
+  };
 
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {data.map((data) => (
-            <TableRow
-              key={data.firstName}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-              <TableCell component="th" scope="row">
-                {data.firstName}
-              </TableCell>
-              <TableCell align="right">{data.lastName}</TableCell>
-              <TableCell align="right">{data.dateOfBirth}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <MUIDataTable title={'Lista pracowników'} data={data} columns={columns} options={options} />
   );
 }
