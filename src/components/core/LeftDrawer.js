96ab20849d8c6e33e68a45f15e9b14bd4c0082ed
Oskar Tybura (s19395) <s19395@pjwstk.edu.pl>
2024-07-16 16:43:41 +0200

    #30 Adapted left drawer so employee form opens

--- src/components/core/LeftDrawer.js
+++ src/components/core/LeftDrawer.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
 import Toolbar from '@mui/material/Toolbar';
@@ -12,17 +12,28 @@ import WorkIcon from '@mui/icons-material/Work';
 import { Link } from 'react-router-dom';
 import DescriptionIcon from '@mui/icons-material/Description';
 import PersonAddAlt1Icon from '@mui/icons-material/PersonAddAlt1';
+import CreateEmployee from '../../pages/NewEmployeeForm';
 
 const drawerWidth = 240;
 
 const items = [
-  { text: 'Nowy pracownik', icon: <PersonAddAlt1Icon /> },
+  { text: 'Nowy pracownik', icon: <PersonAddAlt1Icon />, action: 'openPopup' },
   { text: 'Pracownicy', icon: <PersonIcon />, link: '/employees' },
   { text: 'Stanowiska', icon: <WorkIcon />, link: '/positions' },
   { text: 'Umowy', icon: <DescriptionIcon />, link: '/contracts' }
 ];
 
 export default function LeftDrawer() {
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
     <>
       <Drawer
@@ -35,9 +46,12 @@ export default function LeftDrawer() {
         <Toolbar />
         <Box sx={{ overflow: 'auto' }}>
           <List>
-            {items.map(({ text, icon, link }) => (
+            {items.map(({ text, icon, link, action }) => (
               <ListItem key={text} disablePadding>
-                <ListItemButton component={Link} to={link} key={text}>
+                <ListItemButton
+                  component={link ? Link : 'button'}
+                  to={link ? link : undefined}
+                  onClick={action === 'openPopup' ? handleOpen : undefined}>
                   <ListItemIcon>{icon}</ListItemIcon>
                   <ListItemText primary={text} />
                 </ListItemButton>
@@ -46,6 +60,7 @@ export default function LeftDrawer() {
           </List>
         </Box>
       </Drawer>
+      <CreateEmployee open={open} onClose={handleClose} />
     </>
   );
 }
